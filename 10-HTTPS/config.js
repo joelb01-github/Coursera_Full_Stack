ec168f42417af7ac24f422a615daafc0b8089465
jos <ebrosetup@gmail.com>
2018-10-20 12:08:27 +0100

    added facebook login

--- 10-HTTPS/config.js
+++ 10-HTTPS/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-12345-12345-12345',
-  'mongoUrl': 'mongodb://localhost:27017/conFusion'
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientId: '345484679538349',
+    clientSecret: '8e776ad71e353273d44fa54be0926260'
+  }
 }
